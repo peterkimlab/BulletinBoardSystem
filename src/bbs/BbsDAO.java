628c2e04330272a7b2522839e301ba602f7b29ed
peter <niceguy0825@gmail.com>
2020-10-19 13:46:42 +0900

    JSP 게시판 만들기 강좌 12강 - 게시글 보기기능 구현하기

--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -100,12 +100,34 @@ public class BbsDAO {
 			pstmt.setInt(1, getNext() - (pageNumber -1) * 10);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				return true;
-			}			
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	public Bbs getBbs(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(1));
+				return bbs;
+			}			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
